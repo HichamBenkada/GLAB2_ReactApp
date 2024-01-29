7ea35545fa0ce488db8e96fce3476749ba4988b9
HichamBENKADA <hicham20014@gmail.com>
2024-01-29 13:18:37 -0500

    components are created and App function is updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,30 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+function Header() {
+  return (<h1>Simple React Application</h1>);
+}
+
+function Content(props) {
+  return (<p style={{color: props.color}}>{props.text}</p>);
+}
+
+function Footer() {
+  return (<h1>Created by Me, of course.</h1>);
+}
+
+function App() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+       <Header />
+      <Content color="blue" text="This is my first React Application!" />
+      <Content color="red" text="Wish me luck..." />
+      <Content color="green" text="I think I've got it!" />
+      <Footer />
+        <em emstyle={{border: "0.2px solid blue",borderRadius:"20px"}}>Yesterday is the past, tomorrow is the future, but today is a gift. That is why it is called the Present <strong>~ Bil KEANE</strong></em>
     </>
-  )
+  );
 }
 
 export default App
